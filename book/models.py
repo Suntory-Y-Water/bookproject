cee9f7e1e4ea00df01de93aecdf078c530f3a7ec
sui <1202ayasnppk@gmail.com>
2023-02-12 09:30:25 +0900

    [index] 新規作成

---
+++ book/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+CATEGORY = (('business', 'ビジネス'), ('life', '生活'), ('other', 'その他'))
+
+class Book(models.Model):
+    # データ型の定義
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    category = models.CharField(max_length=100, choices=CATEGORY)
+
+    def __str__(self) -> str:
+        return self.title
