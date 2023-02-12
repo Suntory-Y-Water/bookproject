1b6544ae4f5b9688c40e3f9c092ebda2f68f1346
sui <1202ayasnppk@gmail.com>
2023-02-12 10:52:24 +0900

    [progress] 4-9迄実施

--- book/urls.py
+++ book/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('book/', views.ListBookView.as_view()),
     path('book/<int:pk>/detail/', views.DetailBookView.as_view()),
+    path('book/create/', views.CreateBookView.as_view()),
 ]
